baff581f81ada0787db13b3a21d2103f15d44e4a
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-02-14 20:56:46 +0200

    Set Mantine libraby for building UI

---
+++ src/components/Weather.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '@mantine/core';
+
+const Weather = () => {
+  const a = 0;
+
+  return (
+    <>
+      <Button>Click me! </Button>
+      {a}
+    </>
+  );
+};
+
+export default Weather;
