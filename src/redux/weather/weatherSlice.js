b37a076e76d1de2731fec06a59725b3d93156d7d
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-02-17 22:50:13 +0200

    Set sample array for testing design

--- src/redux/weather/weatherSlice.js
+++ src/redux/weather/weatherSlice.js
@@ -1,60 +1,11 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// import axios from 'axios';
-// import api from '../../api';
-
-// const apikey = '45ef86c3a51d4ca7acc85658231502';
-// const apiKey = 'f0cad4d31278ac0a6fd7a4034450e580';
 const FETCH = 'weather/FETCH';
 
 const initialState = {
   weather: [],
 };
 
-// const randomCities = JSON.stringify({
-//   locations: [
-//     {
-//       q: 'paris',
-//     },
-//     {
-//       q: 'chicago',
-//     },
-//     {
-//       q: 'washington',
-//     },
-//     {
-//       q: 'London',
-//     },
-//     {
-//       q: '90201',
-//     },
-//     {
-//       q: 'kinshasa',
-//     },
-//     {
-//       q: 'nairobi',
-//     },
-//     {
-//       q: 'madrid',
-//     },
-//     {
-//       q: 'tokyo',
-//     },
-//     {
-//       q: 'new york',
-//     },
-//   ],
-// });
-
 export const getWeatherFromRandomCities = createAsyncThunk(FETCH, async () => {
-  // const weather = await api.get(`/current.json?key=${apikey}&q=paris`, { data: randomCities });
-  // const weather = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=bulk`, randomCities);
-
-  // api.get(`/current.json?key=${apikey}&q=bulk`, randomCities).then((dat) => {
-  //   console.log(dat.data);
-  // });
-  // console.log(weather.data);
-  // console.log(randomCities);
-
   const weather = [{
     id: 1, name: 'london', country: 'United Kingdom', localtime: '12:09 PM', humidity: '80%', temp_c: '23',
   }, {
@@ -71,18 +22,6 @@ const weatherSlice = createSlice({
   name: 'weather',
   initialState,
   reducers: {
-    // reserveMission(state, action) {
-    //   const missions = state.missions.map((mission) => {
-    //     if (mission.id === action.payload) {
-    //       return {
-    //         // set the reverse value
-    //         ...mission, reserved: !mission.reserved,
-    //       };
-    //     }
-    //     return mission;
-    //   });
-    //   return { ...state, missions };
-    // },
   },
   extraReducers: {
     [getWeatherFromRandomCities.fulfilled]: (state, action) => {
@@ -92,5 +31,4 @@ const weatherSlice = createSlice({
   },
 });
 
-// export const { reserveMission } = missionsSlice.actions;
 export default weatherSlice.reducer;
