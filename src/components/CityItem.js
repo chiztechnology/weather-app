71be7e2758f90aca9fd2023e68b17fb3e1b3b78f
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-02-24 23:29:32 +0200

    Clear city item component

--- src/components/CityItem.js
+++ src/components/CityItem.js
@@ -1,38 +1,21 @@
 /* eslint-disable */
-import { Image, Text } from '@mantine/core';
-import React, { useEffect } from 'react';
+import { Text } from '@mantine/core';
+import React from 'react';
 import { FaCity } from 'react-icons/fa';
-import { useDispatch, useSelector } from 'react-redux';
-import { createSearchParams, useNavigate } from "react-router-dom";
-import { getWeatherDetails } from '../redux/weather/weatherSlice';
+import { createSearchParams, useNavigate } from 'react-router-dom';
 import './cityitem.css';
-// import { useSelector } from 'react-redux';
 
 const CityItem = (props) => {
-  // const props = {};
-  // console.log(props);
-  const { city, country } = props.data;
   const navigate = useNavigate();
+  const { city, country } = props.data;
 
-  // const { humididy, temp_c } = props.data ? props.data.condition: 0;
-  // const { name, country, localtime } = '';
-  // const { humididy, temp_c } = '';
   return (
-    <div className="item-container" onClick={() => navigate({ pathname: "/details", search: createSearchParams({ cityName: city }).toString() })}>
+    <div className="item-container" onClick={() => navigate({ pathname: '/details', search: createSearchParams({ cityName: city }).toString() })}>
       <Text style={{ alignItems: 'center', display: 'flex' }}>
         <FaCity style={{ paddingRight: 5 }} size={24} />
         {city}
       </Text>
       <Text>{country}</Text>
-      {/* <Text> Local Time: {localtime}</Text>
-      <Text> Temperature: {temp_c}</Text> */}
-      {/* <Image
-        radius="md"
-        src="//cdn.weatherapi.com/weather/64x64/night/143.png"
-        alt="Random unsplash image"
-        style={{ width: 30, height: 'auto' }}
-      />
-      <Text>Humidity : {humidity}</Text> */}
     </div>
   );
 };
